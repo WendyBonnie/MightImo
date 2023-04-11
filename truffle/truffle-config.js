4855ac3aedfbc9a0e30aa93f6dd53b68d8d70222
Wendy Montagnon <wendymontagnon@gmail.com>
2023-04-11 11:48:24 +0200

    mumbai testnet

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -9,6 +9,9 @@ module.exports = {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
+      blockTracker: {
+        pollingInterval: 10000, // Increase the interval to 10 seconds
+      },
     },
     goerli: {
       provider: function () {
